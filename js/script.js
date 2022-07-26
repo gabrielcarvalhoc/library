a50d13acc28896d79529cdc3a99bfadf4c8af3cf
Gabriel <gabrielcarvalho@poli.ufrj.br>
2022-07-26 15:49:13 -0300

    Add function to display books on table

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-let myLibrary = [];
+let tbody = document.querySelector('#books-table');
+
+let myLibrary = [
+    {
+        title: 'test',
+        author: 'test2',
+        pages: 'test3',
+        read: true
+    },
+    {
+        title: 'test4',
+        author: 'test5',
+        pages: 'test6',
+        read: true
+    },
+    {
+        title: 'test7',
+        author: 'test8',
+        pages: 'test9',
+        read: true
+    },
+];
 
 function Book(title, author, pages, read) {
     this.title = title
@@ -7,15 +28,31 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBooktoLibrary() {
-    let title = prompt('Title');
-    let author = prompt('Author');
-    let pages = prompt('Pages');
-    let read = prompt('Read');
+// function addBooktoLibrary() {
+//     let title = prompt('Title');
+//     let author = prompt('Author');
+//     let pages = prompt('Pages');
+//     let read = prompt('Read');
 
-    const book = new Book(title, author, pages, read);
+//     const book = new Book(title, author, pages, read);
     
-    myLibrary.push(book);
+//     myLibrary.push(book);
+// }
+
+function displayBook() {
+    
+    myLibrary.forEach((book) => {
+        let tr = document.createElement('tr');
+        tbody.append(tr);
+        
+        for (let prop in book) {
+            let td = document.createElement('td');
+            tr.append(td);
+            td.append(book[prop]);
+        }
+    })
 }
 
-addBooktoLibrary();
+// addBooktoLibrary();
+
+displayBook();
